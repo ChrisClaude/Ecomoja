b33443f7c73c21538b5a136b1dc0e95a70d49580
Claude De-Tchambila <christ.tchambila@gmail.com>
2021-08-08 22:31:18 +0200

    updated navbar category layout

--- components/layout/header/NavBarProductCategory.tsx
+++ components/layout/header/NavBarProductCategory.tsx
@@ -1,209 +1,241 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 
-const NavBarProductCategory = () => (
+const ShopCategory = () => (
 	<>
-		{/* #region Mobile daily deals */}
-		<div className="daily-deals d-flex align-items-center d-md-none">
-			<div>
-				<FontAwesomeIcon icon={['far', 'clock']} />
-				<a href="#" className="text-white">
-					<span className="text-uppercase">Daily Deals</span>
-				</a>
-			</div>
-			<a href="#" className="text-white font-weight-bold ml-auto">
-				View More
-			</a>
-		</div>
-		{/* #endregion */}
-
-		{/* #region Desktop daily deals */}
-		<div className="banner">
-			<div className="container position-relative py-2">
-				<div className="row">
-					<div className="col-md-12">
-						<div className="d-md-flex">
-							<div className="items-category-container">
-								<div className="items-category">
-									<div className="row no-gutters">
-										<div className="col-md-12">
-											<div className=" items-category-header">
-												Shop by Department
-												<span className="ml-auto">
-													<FontAwesomeIcon icon="chevron-down" />
-												</span>
-											</div>
-										</div>
-										<div className="col-md-12">
-											<ul className="list-group">
-												<li className="list-group-item">
-													<span>Cras justo odio</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Dapibus ac facilisis in</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Morbi leo risus</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Porta ac consectetur ac</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Vestibulum at eros</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Cras justo odio</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Dapibus ac facilisis in</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Morbi leo risus</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Porta ac consectetur ac</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Vestibulum at eros</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Cras justo odio</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Dapibus ac facilisis in</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Morbi leo risus</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Porta ac consectetur ac</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-												<li className="list-group-item">
-													<span>Vestibulum at eros</span>
-													<span>
-														<FontAwesomeIcon icon="chevron-right" />
-													</span>
-												</li>
-											</ul>
-											<a href="#" className="daily-deals">
-												<span>
-													<FontAwesomeIcon icon={['far', 'clock']} />
-												</span>
-												<span className="text-uppercase">Daily Deals</span>
-											</a>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="search-container">
-								<div className="bg-white row no-gutters">
-									<div className="col-md-8">
-										<label htmlFor="search-product" className="w-100 mb-0">
-											<input
-												type="text"
-												className="w-100"
-												placeholder="Search for products, brands..."
-											/>
-										</label>
-									</div>
-									<div className="col-md-3">
-										<label htmlFor="select-department" className="w-100 mb-0">
-											<select className="form-control form-control-sm">
-												<option>All Departments</option>
-												<option>Baby and Toddler</option>
-												<option>Beauty</option>
-												<option>Books</option>
-												<option>Cameras</option>
-												<option>Camping & Outdoors</option>
-												<option>Cellphones & Wearables</option>
-												<option>Computers & Tablets</option>
-												<option>Fashion</option>
-												<option>Gaming</option>
-											</select>
-										</label>
-									</div>
-									<div className="col-md">
-										<button
-											className="btn btn-ecolap-gray w-100 h-100"
-											type="button"
-										>
-											<span>
-												<FontAwesomeIcon icon="search" />
-											</span>
-										</button>
-									</div>
-								</div>
-								<div className="swiper-container mt-2">
-									<ul className="list-group list-group-horizontal row">
-										<li className="list-group-item col-sm-2 text-center swiper-item">
-											Clearance store
-										</li>
-										<li className="list-group-item col-sm-2 text-center swiper-item">
-											Shop local
-										</li>
-										<li className="list-group-item col-sm-1 text-center swiper-item">
-											Winter
-										</li>
-										<li className="list-group-item col-sm-2 text-center swiper-item">
-											Fashion outlet
-										</li>
-										<li className="list-group-item col-sm-2 text-center swiper-item">
-											Brand store
-										</li>
-										<li className="list-group-item col-sm-3 text-center swiper-item">
-											Exclusive to craze environmental
-										</li>
-									</ul>
-								</div>
-							</div>
+		{/*#region Items Category*/}
+		<div className="w-full relative">
+			<div className="absolute w-56 z-20 overflow-hidden bg-white rounded-md shadow-lg">
+				<div className="flex flex-col">
+					<div className="w-full">
+						<div className="flex items-center justify-between relative text-white bg-ecolap-gray py-2 px-5 hover:bg-ecolap-gray-h">
+							Shop by Department
+							<span className="">
+								<FontAwesomeIcon icon="chevron-down" />
+							</span>
 						</div>
+					</div>
+					<div className="w-full">
+						<ul className="flex flex-col my-2">
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Cras justo odio</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Dapibus ac facilisis in</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Morbi leo risus</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Porta ac consectetur ac</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Vestibulum at eros</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Cras justo odio</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Dapibus ac facilisis in</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Morbi leo risus</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Porta ac consectetur ac</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Vestibulum at eros</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Cras justo odio</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Dapibus ac facilisis in</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Morbi leo risus</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Porta ac consectetur ac</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+							<li className="py-1 px-5 flex items-center justify-between hover:bg-primary hover:text-white cursor-pointer">
+								<span>Vestibulum at eros</span>
+								<span>
+									<FontAwesomeIcon icon="chevron-right" />
+								</span>
+							</li>
+						</ul>
+						<a
+							href="#"
+							className="w-full block py-3 px-5 bg-ecolap-green text-white hover:no-underline hover:text-white"
+						>
+							<span className="mr-1 text-base">
+								<FontAwesomeIcon icon={['far', 'clock']} />
+							</span>
+							<span className="text-uppercase text-base font-bold">
+								Daily Deals
+							</span>
+						</a>
 					</div>
 				</div>
 			</div>
 		</div>
-		{/* #endregion */}
+		{/*#endregion */}
+	</>
+);
+
+const SearchContainer = () => (
+	<>
+		{/*#region Search Container */}
+		<div className="search-container">
+			<div className="bg-white flex">
+				<div className="w-3/6">
+					<label htmlFor="search-product" className="w-full">
+						<input
+							type="text"
+							className="w-full"
+							placeholder="Search for products, brands..."
+						/>
+					</label>
+				</div>
+				<div className="w-2/6">
+					<label htmlFor="select-department" className="w-100 mb-0">
+						<select className="form-control form-control-sm">
+							<option>All Departments</option>
+							<option>Baby and Toddler</option>
+							<option>Beauty</option>
+							<option>Books</option>
+							<option>Cameras</option>
+							<option>Camping & Outdoors</option>
+							<option>Cellphones & Wearables</option>
+							<option>Computers & Tablets</option>
+							<option>Fashion</option>
+							<option>Gaming</option>
+						</select>
+					</label>
+				</div>
+				<div className="w-1/6">
+					<button className="btn btn-ecolap-gray w-full h-full" type="button">
+						<span>
+							<FontAwesomeIcon icon="search" />
+						</span>
+					</button>
+				</div>
+			</div>
+			<div className="bg-white overflow-hidden rounded mt-2">
+				<ul className="flex">
+					<li
+						className="flex-1 bg-gray-200 cursor-pointer truncate text-center hover:bg-gray-300 py-2 px-3"
+						title="Clearance store"
+					>
+						Clearance store
+					</li>
+					<li
+						className="flex-1 bg-gray-200 cursor-pointer truncate text-center hover:bg-gray-300 py-2 px-3"
+						title="Shop Local"
+					>
+						Shop local
+					</li>
+					<li
+						className="flex-1 bg-gray-200 cursor-pointer truncate text-center hover:bg-gray-300 py-2 px-3"
+						title="Winter"
+					>
+						Winter
+					</li>
+					<li
+						className="flex-1 bg-gray-200 cursor-pointer truncate text-center hover:bg-gray-300 py-2 px-3"
+						title="Fashion Outlet"
+					>
+						Fashion outlet
+					</li>
+					<li
+						className="flex-1 bg-gray-200 cursor-pointer truncate text-center hover:bg-gray-300 py-2 px-3"
+						title="brand store"
+					>
+						Brand store
+					</li>
+					<li
+						className="flex-1 bg-gray-200 cursor-pointer truncate text-center hover:bg-gray-300 py-2 px-3"
+						title="Exclusive"
+					>
+						Exclusive
+					</li>
+				</ul>
+			</div>
+		</div>
+		{/*#endregion */}
+	</>
+);
+
+const NavBarProductCategory = () => (
+	<>
+		{/*#region Mobile daily deals */}
+		<div className="px-2 flex items-center h-12 justify-between bg-ecolap-green md:px-32 lg:hidden">
+			<div>
+				<FontAwesomeIcon icon={['far', 'clock']} className="text-white mr-1" />
+				<a href="#" className="text-white">
+					<span className="text-uppercase">Daily Deals</span>
+				</a>
+			</div>
+			<a href="#" className="text-white font-weight-bold">
+				View More
+			</a>
+		</div>
+		{/*#endregion */}
+		{/*#region Desktop daily deals */}
+		<div className="hidden px-2 py-2 bg-ecolap-blue lg:flex lg:px-44">
+			<div className="w-2/5">
+				<ShopCategory />
+			</div>
+
+			<div className="w-3/5">
+				<SearchContainer />
+			</div>
+		</div>
+		{/*#endregion */}
 	</>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: 'jit',
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				primary: '#0b79bf',
+				secondary: '#ecc94b',
+				'ecolap-gray': '#4d4d4d',
+				'ecolap-green': '#25CD66',
+				'ecolap-gray-h': '#404042',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
