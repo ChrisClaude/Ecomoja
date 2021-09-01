89b89fa03e9b82249caadcc3c28f32ed878447a0
Claude De-Tchambila <claude.detchambila@dariel.co.za>
2021-09-01 23:43:17 +0200

    updated cart component

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -1,26 +1,98 @@
 import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { UIContext } from '@/utils/context/UIContext';
+import { Button } from '@/components/common';
+import { CartItem as CartItemType } from '@/types/AppTypes';
 import s from './Cart.module.scss';
+import Image from 'next/image';
 
-const Cart = () => {
+const CartNavICon = () => {
 	const { cartItems } = React.useContext(UIContext);
 
 	return (
-		<div className={s.cartWrapper}>
-			<a
-				href="#"
-				className="flex text-white items-center justify-center rounded-3xl w-12 h-8 bg-green-600 hover:bg-green-700 hover:text-white hover:no-underline"
-			>
-				<FontAwesomeIcon icon="shopping-cart" className="w-6" />
-				<span className="ml-1">{cartItems.length}</span>
-			</a>
+		<a
+			href="#"
+			className="flex text-white items-center justify-center rounded-3xl w-12 h-8 bg-green-600 hover:bg-green-700 hover:text-white hover:no-underline"
+		>
+			<FontAwesomeIcon icon="shopping-cart" className="w-6" />
+			<span className="ml-1">{cartItems.length}</span>
+		</a>
+	);
+};
 
-			<div className={s.cartItems}>
-				<div className="bg-green-600 h-40">Cart Content</div>
+const CartItem = ({ cartItem }: { cartItem: CartItemType }) => (
+	<div className="flex px-5 py-2 h-20">
+		<div className="w-1/4 flex items-center justify-center">
+			<Image
+				loader={() => cartItem.product.image}
+				width={50}
+				height={50}
+				src={cartItem.product.image}
+				alt={cartItem.product.name}
+				objectFit="cover"
+			/>
+		</div>
+		<div className="flex flex-col flex-1">
+			<div className="flex flex-1 justify-between">
+				<div>{cartItem.product.name}</div>
+				<div>
+					<button type="button">
+						<span className="material-icons">delete</span>
+					</button>
+				</div>
+			</div>
+			<div className="flex">
+				<span>Qty: {cartItem.productInstances}</span>
+				<span className="ml-auto">R {cartItem.product.currentPrice}</span>
+			</div>
+		</div>
+	</div>
+);
+
+const calculateCartTotal = (cart: CartItemType[]): number => {
+	let total = 0;
+
+	cart.forEach((item) => {
+		total += item.product.currentPrice * item.productInstances;
+	});
+
+	return total;
+};
+
+const CartMini = () => {
+	const { cartItems } = React.useContext(UIContext);
+
+	return (
+		<div className={s.cartMini}>
+			<div>
+				{cartItems.map((item) => (
+					<CartItem cartItem={item} key={item.id} />
+				))}
+			</div>
+			<div>
+				<div className="py-2 px-5 border-t-2 border-b-2 border-solid border-gray-300 flex justify-end font-bold">
+					Total: R {calculateCartTotal(cartItems)}
+				</div>
+				<div className="flex justify-between px-6 my-3">
+					<Button secondary className="bg-green-600 w-1/2 mr-2">
+						<span className="material-icons text-base mr-1">lock</span>
+						<span>Checkout</span>
+					</Button>
+					<Button secondary className="bg-green-600 w-1/2">
+						<span className="material-icons text-base mr-1">shopping_cart</span>
+						<span>Cart</span>
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
 };
+
+const Cart = () => (
+	<div className={s.cartWrapper}>
+		<CartNavICon />
+		<CartMini />
+	</div>
+);
 
 export default Cart;
