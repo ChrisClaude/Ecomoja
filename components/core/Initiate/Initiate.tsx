c26f270e6e9a4098a2511e68a84d2e14cc2faf53
Benelzane <kholani.crazeenvironmental@gmail.com>
2023-04-27 12:29:52 +0200

    fix dispearing cart state

--- components/core/Initiate/Initiate.tsx
+++ components/core/Initiate/Initiate.tsx
@@ -1,16 +1,27 @@
 import * as React from 'react';
 import { ToastContainer } from 'react-toastify';
-import { getCartFromLocalStorage } from '@/helpers/main';
+import { getAllCartItems } from '@/helpers/main';
 import { UIContext } from '@/hooks/context/UIContext';
+import { CartItem } from '@/types/AppTypes';
 
 const Initiate = () => {
 	const { dispatch } = React.useContext(UIContext);
-	React.useEffect(() => {
-		const storedCartItems = getCartFromLocalStorage();
 
-		if (storedCartItems !== null) {
-			dispatch({ type: 'PATCH_CART', payload: storedCartItems });
+	React.useEffect(() => {
+
+		const getCartItems = async()=> {
+			try{
+				const cartItems: CartItem[] = await getAllCartItems();
+			    if (cartItems !== null) {
+					dispatch({ type: 'PATCH_CART', payload: cartItems });
+				}
+			}catch(err){
+				console.log(err);
+			}
+
 		}
+		getCartItems();
+
 	}, []);
 
 	return (
