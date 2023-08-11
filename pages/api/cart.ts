b47c6bca807e67be02908b12af4f486be00531b2
ColeColombia <benzemabenelzane@yahoo.com>
2023-08-11 22:29:32 +0200

    separate code

--- pages/api/cart.ts
+++ pages/api/cart.ts
@@ -30,32 +30,7 @@ export const saveCartItem = async (req, res) => {
 			res.status(403).json({ message: 'Could not add your cart item' });
 		}
 	}
-	else if(req.method === 'GET'){
-		if (req.method === 'GET') {
-			if (!req.headers.cookie) {
-				res.status(403).json({ message: 'Not Authorized' });
-				return;
-			}
-
-			const {id} = req.query
-			const { token } = cookie.parse(req.headers.cookie);
-			const strapiRes = await fetch(`${API_URL}/carts/${id}`, {
-				method: 'GET',
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			});
-	
-			const response = await strapiRes.json();
-	
-			if (strapiRes.ok) {
-				res.status(200).json({ response });
-			} else {
-				res.status(403).json({ message: 'Could not add your cart item' });
-			}
-		}
-
-	} else {
+	else {
 		res.setHeader('Allow', ['GET']);
 		res.status(405).json({ message: `Method ${req.method} not allowed` });
 	}
